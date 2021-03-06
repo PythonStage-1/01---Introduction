eb83b636046063ef3a58b84d045e8e4f629f5c23
ProSamuraidev <76863455+ProSamuraidev@users.noreply.github.com>
2021-03-06 10:12:03 +0330

    Add files via upload

---
+++ E01-Stage-1.py
@@ -0,0 +1,26 @@
+# myStr = "String Mystr" 
+# print(type(myStr))
+# myInt = 2021
+# print(type(myInt))
+# myFl = 20.21
+# print(type(myFl))
+# myLis = [1, 2.0, 'AmirHosseinKhorram']
+# print(type(myLis))
+# #User, market: Digikala SabadKharid: [mouse, keyboard, TV, Laptop] 
+# #Max Price: TV 5.000.000 t
+# #Min Price: mouse 200.000 t
+# myTuple = (1, 2.0, 'AmirHosseinKhorram')
+# print(type(myTuple))
+# mySets = set([1, 2.0, 'amirhosseinkhorram'])
+# print(type(mySets))
+
+# myDict = {'user': 'AmirHossein', 'Password': 'Hello!123'} 
+# print(type(myDict))
+
+# Name = "Amir Hossein"
+# LastName = "Khorram"
+# Age = 19
+# print(Name, LastName, Age) Name: Amir Hossein LastName: Khorram Age: 19
+# print(f"Name:{Name} LastName:{LastName} Age: {Age}")
+# print("Name: %s" %Name)
+# print("Name {} LastName {} Age {}".format(Name, LastName, Age))
